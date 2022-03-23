1c3e6aad1c08d3a30570abf43670bfae222f7d73
Rahul <pratibhaayanrahul@gmail.com>
2022-03-23 21:44:21 +0530

    Change main

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import os
 
 API_KEY = os.environ.get("NEWS_KEY")
 URL = f"https://newsapi.org/v2/top-headlines?country=in&apiKey={API_KEY}"
+URL_2 = f"https://newsapi.org/v2/top-headlines?country=au&apiKey={API_KEY}"
 
 app = Flask(__name__)
 Bootstrap(app)
@@ -15,6 +16,13 @@ def home():
     response = requests.get(url=URL)
     all_news = response.json()["articles"]
     return render_template("index.html", all_news=all_news)
+
+
+@app.route('/au')
+def home():
+    response = requests.get(url=URL_2)
+    all_news = response.json()["articles"]
+    return render_template("aus_news.html", all_news=all_news)
 
 
 if __name__ == "__main__":
